0997de45bde400228c45f62eab0c95234197bcdf
Carl Pulley <carlp@cakesolutions.net>
2015-08-31 16:46:19 +0100

    Stage 1 development

--- src/main/scala/cakesolutions/Main.scala
+++ src/main/scala/cakesolutions/Main.scala
@@ -1,26 +1,17 @@
 package cakesolutions
 
 import akka.actor.ActorSystem
-import akka.http.scaladsl.Http
-import akka.stream.ActorMaterializer
-import akka.util.Timeout
-import java.util.concurrent.TimeUnit
-import scala.concurrent.duration._
+import cakesolutions.menu.ValidatedLoad
 
-object Main extends App with Service {
+object Main extends App with ValidatedLoad {
 
   implicit val system = ActorSystem("KebabApplication")
-  implicit val materializer = ActorMaterializer()
 
   val config = system.settings.config
-  val hostname = config.getString("hostname")
-  val port = config.getInt("port")
   val log = system.log
-
-  implicit val timeout = Timeout(config.getDuration("timeout", TimeUnit.SECONDS).seconds)
 
   log.info("We started!")
 
-  val bindingFuture = Http().bindAndHandle(route, hostname, port)
+  log.info(s"Loaded menu items: ${load(config)}")
 
 }
--- src/main/scala/cakesolutions/menu/ValidatedLoad.scala
+++ src/main/scala/cakesolutions/menu/ValidatedLoad.scala
@@ -6,37 +6,12 @@ import scala.util.{Failure, Success, Try}
 
 trait ValidatedLoad {
 
-  private[this] def loadItem(item: Config): Try[Item] = {
-    if (! item.hasPath("name") || ! item.hasPath("price")) {
-      return Failure(new Exception(s"Misconfiguration detected - menu item $item is missing a `name` or a `price`"))
-    }
-
-    val description = item.getString("name")
-    val price = item.getDouble("price")
-
-    Try(Item(description, price))
-  }
-
   /**
    * Validates the loaded configuration object to return a validated menu.
    *
    * @param config configuration object that `Order` will load and validate menu items from
    * @return validated list of menu items
    */
-  def load(config: Config): Try[Order] = {
-    if (! config.hasPath("menu.items")) {
-      return Failure(new Exception("Misconfiguration detected - could not locate path `menu.items`"))
-    }
-
-    val items = config.getConfigList("menu.items")
-
-    items.foldRight[Try[Order]](Success(Order(Vector.empty))) {
-      case (_, err: Failure[Order]) =>
-        err
-
-      case (item, Success(Order(order))) =>
-        for (newItem <- loadItem(item)) yield Order(newItem +: order)
-    }
-  }
+  def load(config: Config): Try[Order] = ???
 
 }
